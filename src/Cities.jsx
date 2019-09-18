25f7ff1a6b820d50eb0860e276a2fd6010247452
Joe Ganobsik <jganobsik@gmail.com>
2019-09-17 22:29:05 -0400

    add query to cities

--- src/Cities.jsx
+++ src/Cities.jsx
@@ -1,6 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
 export default class Cities extends Component {
+    <Query query={gql`
+    query JobsByCity{
+        cities {
+          name
+        jobs{
+        title
+        applyUrl
+        company{
+          name
+          websiteUrl
+        }
+        }
+      }
+    }
+    `
+    }
     render() {
         return (
             <div>
