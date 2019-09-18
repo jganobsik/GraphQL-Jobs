c7e357772d2a4983409b9adb4e2ef88977fc518f
Joe Ganobsik <jganobsik@gmail.com>
2019-09-17 22:43:27 -0400

    test query via log

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import ApolloClient from 'apollo-boost';
+import ApolloClient, { gql } from 'apollo-boost';
 import './App.css';
 import {ApolloProvider} from 'react-apollo';
+
 
 const client = new ApolloClient({
   uri: "https://api.graphql.jobs/"
 });
 
+client.query({query: gql`{
+cities {
+  name
+jobs{
+title
+applyUrl
+company{
+  name
+  websiteUrl
+}
+}
+}
+}
+`
+}).then(r => console.log(r))
 const App = () => (
 <ApolloProvider client={client}>
   <div>
--- src/Cities.jsx
+++ src/Cities.jsx
@@ -1,24 +1,8 @@
 import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
 
-export default class Cities extends Component {
-    <Query query={gql`
-    query JobsByCity{
-        cities {
-          name
-        jobs{
-        title
-        applyUrl
-        company{
-          name
-          websiteUrl
-        }
-        }
-      }
-    }
-    `
-    }
+
+
+export class Cities extends Component { 
     render() {
         return (
             <div>
